01bda8484b7aeab65e6f1f7299bb434d14f9b197
vaibsvikas <99389229+vaibsvikas@users.noreply.github.com>
2023-09-08 22:37:00 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,14 +1,3 @@
 # -*- coding: utf-8 -*-
 
-"""Implementation of the Nelson-Siegel-Svensson interest rate curve model.
-For details, see classes `NelsonSiegelCurve` and `NelsonSiegelSvenssonCurve`.
-"""
-
-__author__ = """luphord"""
-__email__ = "luphord@protonmail.com"
-__version__ = "0.5.0"
-
-from .ns import NelsonSiegelCurve
-from .nss import NelsonSiegelSvenssonCurve
-
-__all__ = ["NelsonSiegelCurve", "NelsonSiegelSvenssonCurve"]
+"""Unit test package for nelson_siegel_svensson."""
